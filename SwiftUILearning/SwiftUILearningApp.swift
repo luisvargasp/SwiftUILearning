8420691af88367e3d84b50610e7304c0d2fde766
Fernando V <lvargasp@uni.pe>
2025-03-24 18:53:17 -0500

    Initial Commit

---
+++ SwiftUILearning/SwiftUILearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearningApp.swift
+//  SwiftUILearning
+//
+//  Created by FerVP on 24/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
